e0b7f06eb5577d06024bba17df4be5f771901a5b
promekid <144685836+promekid@users.noreply.github.com>
2025-09-02 12:51:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
+#include <format>
+#include <limits>
+#include <cctype>
 
-using namespace std;
 
 int main()
 {
     double celcius, kelvin=0.0, farenheight=0.0;
     std::cout << "Converter from celcius to kelvin & fahrengheit" << std::endl;
     std::cout << "==================================================" << std::endl;
-    std::cout << "Insert degrees Celcius: ";
-    std::cin >> celcius;
-    kelvin = celcius + 273,15;
-    farenheight = celcius * (9/5) + 32;
-    std::cout << std::format("|{:^15}|{:^15}|{:^15}|\n", "Celcius", "Kelvin", "Fahrenheit");
-    std::cout << std::format("|{:^15}|{:^15}|{:^15}|\n", celcius, kelvin, farenheight);
+    std::cout << "type ctrl + z to quit\n";
+
+    while(true)
+    {
+        std::cout << "Insert degrees Celcius: ";
+
+        if(!(std::cin >> celcius))
+        {
+            if(std::cin.eof()) break;
+            std::cout << "Invalid input! Please enter a number.\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
+        }
+
+        kelvin = celcius + 273.15;
+        farenheight = celcius * 9.0/5.0 + 32.0;
+        std::cout << std::format("|{:^15}|{:^15}|{:^15}|\n", "Celcius", "Kelvin", "Fahrenheit");
+        std::cout << std::format("|{:^15.5}|{:^15.5}|{:^15.5}|\n", celcius, kelvin, farenheight);
+    }
     return 0;
 }
